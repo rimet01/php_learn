7ca020963c86cf38720184e42b4ab0ed682659a4
rimet01 <rmetzner@proton.me>
2025-08-23 18:47:46 +0200

    Basics

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 // prints ... //
-echo "Hello, World!";
+// This PHP script adds two numbers and displays the result
+$a = 5; // First number
+$b = 7; // Second number
+$sum = $a + $b; // Calculate the sum
+echo "The sum of $a and $b is $sum.";
 ?>
 
 - - -
 
+<?php
+echo "Hello, World!";
+$a = 5
+$b = 6;
+$sum = $a + $b;
+echo "The sum of $a and $b is $sum.";
+?>
