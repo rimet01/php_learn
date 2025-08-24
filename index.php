ea0d885877c9b6e2755028572f968a6fae939440
rimet01 <rmetzner@proton.me>
2025-08-24 21:22:08 +0200

    Enter numbers

--- index.php
+++ index.php
@@ -12,3 +12,13 @@ $b = 6;
 $sum = $a + $b;
 echo "The sum of $a and $b is $sum.";
 ?>
+
+- - -
+
+<?php
+echo "Enter the first number: ";
+$a = trim(fgets(STDIN));
+$b = trim(fgets(STDIN));
+$sum = $a + $b;
+echo "The sum of $a and $b is $sum.";
+?>
